5d02e67072a89acb6341b361a3236cbeb45db1c3
Виктор Щеглов <VAleShcheglov@sberbank.ru>
2023-06-18 17:11:28 +0300

    done

--- src/02/brain.sh
+++ src/02/brain.sh
@@ -1,149 +1,37 @@
 #!/bin/bash
 
-function start {
-    touch log.txt
-    START=$(date +'%s%N')
-    START_TIME=$(date +'%Y-%m-%d %H:%M:%S')
-    echo "Script started at $START_TIME" > log.txt
-    echo "Script started at $START_TIME"
-    count_dirs=$(echo $(( 1 + $RANDOM % 100 )))
-    do_not_enter='\/[s]?bin'
-    for (( number=0; number<$count_dirs; number++ ))
-    do
-        cd /
-        path="/"
-        dir_name=""
-        create_dir
-    done
-    stop_script
+source check.sh
+
+function s21_clean_one {
+  cp ../02/log.txt .
+  LIST_OF_FOLDERS=`cat log.txt | awk -F " " '{ print $1 }'`
+  for var in $LIST_OF_FOLDERS
+  do
+    rm -rf $var
+  done
+  rm -rf log.txt
 }
 
-function create_dir {
-    count_dirs_in_current_dir=$(echo $(ls -l -d */ 2>/dev/null | wc -l ))
-
-    if [[ $count_dirs_in_current_dir -eq 0 ]]
-    then
-        random_number=0
-    else
-        random_number=$(echo $(( $RANDOM % $count_dirs_in_current_dir )))
-    fi
-
-    if [[ $random_number -eq 0 ]]
-    then
-        if [[ ${#letters_dirs} -lt 5 ]]
-        then
-            create_dir_with_little_name
-        else
-            create_dir_with_big_name
-        fi
-        sudo mkdir $path$dir_name 2>/home/tgeodude/proj/Linux-Monitoring-v2/src/02/error.txt
-        if ! [[ -s /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error.txt ]]
-        then
-            echo $path$dir_name --- $(date +'%Y-%m-%d %H:%M:%S') --- >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
-            create_files
-        fi
-    else
-        path+="$( ls -l -d */ | awk '{print $9}' | sed -n "$random_number"p )"
-        if ! [[ $path =~ $do_not_enter ]]
-        then
-            cd $path
-            create_dir
-        fi
-    fi
+function s21_clean_two {
+  cp ../02/log.txt .
+  read -p "Enter the date. For example: YYYY-MM-DD   " arg3
+  read -p "Enter the time begin. For example: HH:MM  " arg1
+  read -p "Enter the time end. For example: HH:MM  " arg2
+  s21_check_date $arg1 $arg2 $arg3
+  if [[ $? -ne 2 ]]
+  then
+    find / -newerct "$arg3 $arg1" ! -newerct "$arg3 $arg2" -type d  2>/dev/null | grep $(date +"%d%m%y") | sudo xargs rm -rf 2>/dev/null
+  fi
+  rm -rf log.txt
 }
 
-function create_dir_with_little_name {
-    count=${#letters_dirs}
-    for (( i=0; i<6-count; i++ ))
-    do
-        dir_name+="$(echo ${letters_dirs:0:1})"
-    done
-    dir_name+="$(echo ${letters_dirs:1:${#letters_dirs}})"
-    dir_name+=$number
-    dir_name+="_"
-    dir_name+=$(date +"%d%m%y")
-}
-
-function create_dir_with_big_name {
-    dir_name=$letters_dirs
-    dir_name+=$number
-    dir_name+="_"
-    dir_name+=$(date +"%d%m%y")
-}
-
-function create_files {
-    file_name_start="$(echo $letters_files | awk -F "." '{print $1}')"
-    file_name_end="$(echo $letters_files | awk -F "." '{print $2}')"
-    count_files=$(echo $(( $RANDOM % 1000 )))
-    for (( number_files=0; number_files <$count_files; number_files++ ))
-    do
-        file_name=""
-        if [[ ${#file_name_start} -lt 5 ]]
-        then
-            if ! [[ -s /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt ]]
-            then
-                create_files_with_little_name
-                echo $path$dir_name"/"$file_name --- $(date +'%Y-%m-%d %H:%M:%S') --- $size "Mb" >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
-            else
-                rm -rf /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt
-                break
-            fi
-        else
-            if ! [[ -s /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt ]]
-            then
-                create_files_with_big_name
-                echo $path$dir_name"/"$file_name --- $(date +'%Y-%m-%d %H:%M:%S') --- $size "Mb" >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
-            else
-                rm -rf /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt
-                break
-            fi
-        fi
-    done
-}
-
-function create_files_with_little_name {
-    count=${#file_name_start}
-    for (( i=0; i<6-count; i++ ))
-    do
-        file_name+="$(echo ${file_name_start:0:1})"
-    done
-    file_name+="$(echo ${file_name_start:1:${#file_name_start}})"
-    file_name+=$number_files
-    file_name+="_"
-    file_name+=$(date +"%d%m%y")
-    file_name+="."
-    file_name+=$file_name_end
-    sudo fallocate -l $size"MB" $path/$dir_name/$file_name 2>/home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt
-    if [[ $(df / -BM | grep "/" | awk -F"M" '{ print $3 }') -le 1024 ]]
-    then
-        stop_script
-        exit 1
-    fi
-}
-
-function create_files_with_big_name {
-    file_name+="$(echo ${file_name_start:0:${#file_name_start}})"
-    file_name+=$number_files
-    file_name+="_"
-    file_name+=$(date +"%d%m%y")
-    file_name+="."
-    file_name+=$file_name_end
-    if [[ $(df / -BM | grep "/" | awk -F"M" '{ print $3 }') -le 1024 ]]
-    then
-        stop_script
-        exit 1
-    fi
-}
-
-function stop_script {
-    rm -rf /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error.txt
-    rm -rf /home/tgeodude/proj/Linux-Monitoring-v2/src/02/error1.txt
-    END=$(date +'%s%N')
-    END_TIME=$(date +'%Y-%m-%d %H:%M:%S')
-    echo "Script finished at $END_TIME" >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
-    echo "Script finished at $END_TIME"
-    DIFF=$((( $END - $START ) / 100000000 ))
-    echo "Script execution time (in seconds) = $(( $DIFF / 10 )).$(( $DIFF % 10 ))" >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
-    echo "Script execution time (in seconds) = $(( $DIFF / 10 )).$(( $DIFF % 10 ))"
-    echo -e "" >> /home/tgeodude/proj/Linux-Monitoring-v2/src/02/log.txt
+function s21_clean_three {
+  read -p "Enter the folder name. For example: folder_031222  " arg1
+  BEGIN_NAME=`echo $arg1 | awk -F "_" '{ print $1 }'`
+  END_NAME=`echo $arg1 | awk -F "_" '{ print $2 }'`
+  
+  BEGIN_NAME+="*"
+  BEGIN_NAME+="_"
+  BEGIN_NAME+=$END_NAME
+  find / -name $BEGIN_NAME -exec rm -rf {} \; 2>/dev/null
 }
